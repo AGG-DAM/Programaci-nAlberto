dcd4c3ff3cae1bde559684b9c5f512d0e0820eac
Alberto <alberto.gargar@iesjulianmarias.es>
2024-10-21 21:23:59 +0200

    Ejercicio 4 modificado

--- UT2/src/Ejercicio4.java
+++ UT2/src/Ejercicio4.java
@@ -2,15 +2,16 @@ import java.util.Scanner;
 
 public class Ejercicio4 {
     public static void main(String[] args) {
-        double numdouble;
+        double numDouble;
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingrese un numero para ver si es cercano a 0 ");
-        numdouble = sc.nextDouble();
+        numDouble = sc.nextDouble();
 
-        if (numdouble >-1 && numdouble <1 && numdouble !=0) {
+        if (numDouble > -1 && numDouble < 1 && numDouble != 0) {
             System.out.println("El numero es cercano a 0");
         } else {
             System.out.println("El numero no es cercano a 0");
         }
     }
 }
+
