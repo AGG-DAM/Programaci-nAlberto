9328ec59ebe035eda11f7b92b1f28b004cb169d4
Alberto <alberto.gargar@iesjulianmarias.es>
2024-10-21 19:51:17 +0200

    Ejercicio 13

---
+++ UT2/src/Ejercicio13.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Ejercicio13 {
+    public static void main(String[] args) {
+        int numero;
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese un numero para saber si es par y divisible entre 5 ");
+        numero = entrada.nextInt();
+        if (numero % 2 == 0 && numero % 5 == 0) {
+            System.out.println("El numero es par y divisible entre 5 ");
+        } else if (numero % 2 == 0) {
+            System.out.println("El numero es par");
+        } else if (numero % 5 == 0) {
+            System.out.println("El numero es  divisible entre 5 ");
+        } else if (numero % 2 == 1) {
+            System.out.println("El numero es impar");
+
+        }
+
+    }
+}
